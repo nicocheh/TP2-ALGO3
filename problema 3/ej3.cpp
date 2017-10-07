d5c51856e6b005e0b92e12ab321c78855f571a0e
Lucas <Lucas>
2017-10-07 02:53:44 -0300

    Deberia estar. Lo pusheo y lo pruebo en la net

--- problema 3/ej3.cpp
+++ problema 3/ej3.cpp
@@ -34,19 +34,55 @@ void agregarRuta(ruta nuevaRuta, vector<ruta>& rutasTotales, vector<ruta>& rutas
 	}
 }
 
-int minPeso(vector<ruta> rutas){
-	//ruta res;
+int minPeso(vector<ruta> rutasPosibles){//devuelve el numero de la ruta con menos peso
+	int res;
 	int pesoMin= infinito;
 	for (int i=0; i<rutas.size(); ++i){
 		if (rutas[i].peso<pesoMin){
-			res=i;
+			res=rutas[i].numero;
 			pesoMin=rutas[i].peso;
 		}
 	}
 	return res;
 }
 
+bool estaEnAGM(vector<nodo> nodosAGM, nodo ciudad){
+    return find(nodosAGM.begin(), nodosAGM.end(), ciudad) != nodosAGM.end();
+}
+
+void eliminarRutas(ruta rutaMinima, vector<ruta>& rutasPosibles, bool agreguePrimera){ //eliminar de rutasPosibles las asociadas a la ciudad nueva con las ciudades viejas
+    if (agreguePrimera){
+        for (int j=0; j<rutaMinima.nodosQueConecta.fst.rutasDe.size(); ++j){//recorre las rutas que salen de la nueva ciudad que agregamos
+            if (estaEnAGM(nodosAGM, rutaMinima.nodosQueConecta.fst.rutasDe[j].nodosQueConecta.fst) && estaEnAGM(nodosAGM, rutaMinima.nodosQueConecta.fst.rutasDe[j].nodosQueConecta.snd)){
+            //si la j-esima ruta que sale del nuevo nodo que agregamos une dos nodos que estan en el AGM
+                //entonces borramos esa ruta de las rutasPosibles
+                rutasPosibles.erase(rutasPosibles.begin()+j-1);
+            }
+        }
+    }else{
+        for (int j=0; j<rutaMinima.nodosQueConecta.snd.rutasDe.size(); ++j){
+            if (estaEnAGM(nodosAGM, rutaMinima.nodosQueConecta.snd.rutasDe[j].nodosQueConecta.fst) && estaEnAGM(nodosAGM, rutaMinima.nodosQueConecta.snd.rutasDe[j].nodosQueConecta.snd)){
+            //si la j-esima ruta que sale del nuevo nodo que agregamos une dos nodos que estan en el AGM
+                //entonces borramos esa ruta de las rutasPosibles
+                rutasPosibles.erase(rutasPosibles.begin()+j-1);
+            }
+        }        
+    }
+}
+
+
+
+
+
+
+
+
+
+
 int main(){
+int f, c, r;
+cin>>f>>c>>r;
+while(f != 0){
 	vector<nodo> nodos;//nodos totales: estan ORDENADOS de forma rara: [0]=fabricas; de 1 a c son los clientes
     //ORDENADO por ID-f de nodo
     vector<nodo> nodosAGM;//nodos que se van guardando en el AGM. Al final deberia tener los mismos elementos que el anterior
@@ -58,9 +94,9 @@ int main(){
     vector<ruta> rutasPosibles;//guarda las rutas que tienen un extremo en el AGM y el otro en Totales
     //VARIABLE todo el tiempo
     
-    int f, c, r, e1, e2, l;
-    int nro=0;//a cada ruta la identificamos con nro (0<=nro<r)
-    cin>>f>>c>>r;
+    int e1, e2, l;
+    int costoTotal=0;
+
     nodo fabricas;
     nodos.push_back(fabricas);				//el elemento 0 es el de las fabricas y el resto sigue la numeracion de c-f
     for (int i=f+1; i<=f+c; ++i){										//para todos los clientes
@@ -71,35 +107,53 @@ int main(){
     assert(nodos.size()==c+1);
     
     cin>>e1>>e2>>l;
-    while(e1 != 0){
+    for (int i=0; i<r; ++i){//inserta las rutas en rutasTotales
         if (e1>f || e2>f){//si ambos son menores (ie, conecta solo fabricas) no nos interesa hacer una ruta entre dos nodos con fabricas
 			ruta nuevaRuta;
-			nuevaRuta.numero=nro;
+			nuevaRuta.numero=i;                     //a cada ruta la identificamos con nro (0<=numero<r)
 			nuevaRuta.peso=l;
 			nuevaRuta.nodosQueConecta(make_pair(e1,e2));
             agregarRuta(nuevaRuta, rutasTotales, rutasPosibles, f, e1, e2);
-			++nro;
         }
         cin>>e1>>e2>>l;
     }
-    assert(nro==r-1);
     
-    for(int i=f+1; i<=f+c; ++i){//este es el ciclo posta del semiprim
+    for(int i=f+1; i<=f+c; ++i){//este es el ciclo posta del casi prim
         int numRuta = minPeso(rutasTotales);
-        rutasAGM.push_back(rutasTotales[i]);
-        ///vec.erase(vec.begin() + i); //elimina la posicion i, ie, el elemento i+1
+        rutasAGM.push_back(rutasTotales[numRuta]);
+        costoTotal+=rutasTotales[numRuta].peso;
+        
+        bool agreguePrimera;
+        if (estaEnAGM(nodosAGM, rutasTotales[numRuta].nodosQueConecta.fst)){
+        //si la primera ciudad de la ruta es la que esta en el agm
+            //entonces que agregue la segunda ciudad al AGM
+            nodosAGM.push_back(rutasTotales[numRuta].nodosQueConecta.snd);
+            agreguePrimera=false;
+        }else{//si la segunda ciudad de la ruta es la que esta en el agm
+            //entonces que agregue la primera ciudad al AGM
+            nodosAGM.push_back(rutasTotales[numRuta].nodosQueConecta.fst);
+            agreguePrimera=true;
+        }
+        eliminarRutas(rutasTotales[numRuta], rutasPosibles, agreguePrimera);
         /*
          * tomar el e de minimo peso de rutas
          * agregar e a rutasAGM
          * agregar la nueva ciudad a nodosAGM?
-         * eliminar de rutas las asociadas a la ciudad nueva con las ciudades viejas
+         * eliminar de rutasPosibles las asociadas a la ciudad nueva con las ciudades viejas
          * 
          */
     }
     
     assert(nodos.size()==nodosAGM.size());
-    assert();
-    
-	return 0;
+
+    cout<<costoTotal<<" "<<rutasAGM.size();
+    for (int k=0; k<rutasAGM.size(); ++k){
+        cout<<" "<<rutasAGM[k].nodosQueConecta.fst<<" "<<rutasAGM[k].nodosQueConecta.snd;
+    }
+    cout<<endl;
+cin>>f>>c>>r;
+}
+
+return 0;
 }
 
